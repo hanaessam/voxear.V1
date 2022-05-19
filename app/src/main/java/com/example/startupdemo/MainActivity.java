98497b21c8575651bb7c6de792a092f10c0a823a
hanaessam <hanaessamsayed@gmail.com>
2022-05-19 12:33:58 +0200

    first commit

--- app/src/main/java/com/example/startupdemo/MainActivity.java
+++ app/src/main/java/com/example/startupdemo/MainActivity.java
@@ -14,69 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.findBtn);
-        Button button4 = findViewById(R.id.adjustAlarm);
-        Button button5 = findViewById(R.id.spaekbtn);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                openSignUpActivity();
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SplashScreen.class);
+                startActivity(i);
+                finish();
             }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSignInActivity();
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFindMyDeviceActivity();
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAdjustAlarmActivity();
-            }
-        });
-
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSpeakActivity();
-            }
-        });
-
+        }, 3000);
     }
 
-    public void openSignInActivity() {
-        Intent intent = new Intent(this, SignIn.class);
-        startActivity(intent);
-    }
-    public void openSignUpActivity() {
-        Intent intent2 = new Intent(this, SignUp.class);
-        startActivity(intent2);
-    }
-    public void openFindMyDeviceActivity() {
-        Intent intent3 = new Intent(this, FindMyDevice.class);
-        startActivity(intent3);
-    }
-
-    public void openSpeakActivity() {
-        Intent intent5 = new Intent(this, Speak.class);
-        startActivity(intent5);
-    }
-    public void openAdjustAlarmActivity() {
-        Intent intent4 = new Intent(this, AdjustAlarm.class);
-        startActivity(intent4);
-    }
 }
 
--- app/src/main/java/com/example/startupdemo/SplashScreen.java
+++ app/src/main/java/com/example/startupdemo/SplashScreen.java
@@ -3,6 +3,8 @@ package com.example.startupdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +14,69 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
+
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.findBtn);
+        Button button4 = findViewById(R.id.adjustAlarm);
+        Button button5 = findViewById(R.id.spaekbtn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
-                finish();
+            public void onClick(View v) {
+                openSignUpActivity();
             }
-        }, 3000);
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignInActivity();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFindMyDeviceActivity();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAdjustAlarmActivity();
+            }
+        });
+
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSpeakActivity();
+            }
+        });
+
+    }
+
+    public void openSignInActivity() {
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+    }
+    public void openSignUpActivity() {
+        Intent intent2 = new Intent(this, SignUp.class);
+        startActivity(intent2);
+    }
+    public void openFindMyDeviceActivity() {
+        Intent intent3 = new Intent(this, FindMyDevice.class);
+        startActivity(intent3);
+    }
+
+    public void openSpeakActivity() {
+        Intent intent5 = new Intent(this, Speak.class);
+        startActivity(intent5);
+    }
+    public void openAdjustAlarmActivity() {
+        Intent intent4 = new Intent(this, AdjustAlarm.class);
+        startActivity(intent4);
     }
 }
